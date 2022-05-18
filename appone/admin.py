3385edc158f0bec3c5aa92d25a2d58b3d2308069
ARTHUR MEDEIROS PAIVA <20211014040004@ifrn.local>
2022-05-18 16:46:21 -0300

    Admin interface

--- appone/admin.py
+++ appone/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from appone.models import Question, Choice
 
 # Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
--- appone/models.py
+++ appone/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Order(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    creator_user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
-    job_user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="job")
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Data de publicação')
+
+    def __str__(self):
+        return f"{self.question_text}"
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.choice_text}"
