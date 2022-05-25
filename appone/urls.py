8c70a31eace11a806b39833a90fc2488e2b399cd
ARTHUR MEDEIROS PAIVA <20211014040004@ifrn.local>
2022-05-25 17:25:47 -0300

    Fluxo de votacao

--- appone/urls.py
+++ appone/urls.py
@@ -6,4 +6,7 @@ import appone.views.auth as auth_views
 urlpatterns = [
     path('questions', question_views.index, name='questions_index'),
     path('questions/<int:id>', question_views.show, name='questions_show'),
+    path('questions/<int:id>/vote', question_views.vote, name='questions_vote'),
+    path('questions/<int:id>/vote/new', question_views.vote_post, name='questions_vote_post'),
+    path('questions/<int:id>/result', question_views.result, name='questions_result'),
 ]
